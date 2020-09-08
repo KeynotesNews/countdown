02c12e9abdd57a13aa1e31e5e512394ad26e2749
Noah Evans <noah@itsnoahevans.co.uk>
2020-09-08 17:37:13 +0100

    Initial code for one week countdown

---
+++ scripts/3-days.py
@@ -0,0 +1,24 @@
+import tweepy
+import os
+import api
+
+auth = tweepy.OAuthHandler(api.consumerKey, api.consumerSecret)
+auth.set_access_token(api.accessKey, api.accessSecret)
+api = tweepy.API(auth)
+print('Connected to APIs')
+
+print('DATE - 12th September')
+image = '/root/countdown/images/3-days.png'
+print('Uploading image...')
+media = api.media_upload(image)
+
+tweet = "Three days until the #AppleEvent!"
+
+for attempt in range(5):
+    try:
+        api.update_status(status=tweet, media_ids=[media.media_id])
+        print('TWEETED:', tweet)
+        break
+    except:
+        print('Error tweeting')
+        continue
---
+++ scripts/6-days.py
@@ -0,0 +1,24 @@
+import tweepy
+import os
+import api
+
+auth = tweepy.OAuthHandler(api.consumerKey, api.consumerSecret)
+auth.set_access_token(api.accessKey, api.accessSecret)
+api = tweepy.API(auth)
+print('Connected to APIs')
+
+print('DATE - 9th September')
+image = '/root/countdown/images/6-days.png'
+print('Uploading image...')
+media = api.media_upload(image)
+
+tweet = "Six days until the #AppleEvent!"
+
+for attempt in range(5):
+    try:
+        api.update_status(status=tweet, media_ids=[media.media_id])
+        print('TWEETED:', tweet)
+        break
+    except:
+        print('Error tweeting')
+        continue
